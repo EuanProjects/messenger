cf85a2cb92739b3b08c219a9d7ffc619f3a8f479
ejcanoy <euan.james.canoy@gmail.com>
2024-07-11 15:10:00 -0700

    Added login feature

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,22 +1,59 @@
+import { useState } from "react"
 import { MessageSquare } from "react-feather"
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 
 function Login() {
+    const API_URL = import.meta.env.VITE_API_URL;
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const navigate = useNavigate();
+
+    async function handleLoginSubmit(e) {
+        e.preventDefault();
+
+        try {
+            const response = await fetch(`http://${API_URL}/login/`, {
+                mode: 'cors',
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    username,
+                    password
+                })
+            });
+
+            const data = await response.json();
+            localStorage.setItem('token', data.token);
+            if (data.setup) {
+                navigate("/home/chats");
+            } else {
+                // go to setup page
+                console.log("Going to setup page");
+            }
+
+        } catch (error) {
+            console.error("Error fetching data: ", error)
+        }
+    }
+    
     return (
         <div className="w-screen h-screen flex flex-col justify-center place-items-center">
             <div className="flex place-items-center gap-4">
                 <h1 className="text-5xl text-deep-purple font-bold  mb-2">Messenger</h1>
                 <MessageSquare className="stroke-deep-purple fill-deep-purple" size={48}/>
             </div>
-            <form className="w-1/4 h-1/2 max-h-[350px] max-w-[318px] bg-grey rounded-lg shadow-lg p-4 flex flex-col place-items-center"> 
+            <form className="h-[350px] w-[318px] bg-grey rounded-lg shadow-lg p-4 flex flex-col place-items-center"
+                onSubmit={handleLoginSubmit}> 
                 <legend className="text-white text-center text-2xl mb-2 font-bold">Login</legend>
                 <p className="text-white grid mb-1">
                     <label className="mr-4" htmlFor="">Username</label>
-                    <input type="text" className="w-[250px]"/>
+                    <input type="text" className="w-[250px] px-2 text-dark-grey" onChange={(e) => setUsername(e.target.value)}/>
                 </p>
                 <p className="text-white my-2 grid mb-6">
                     <label className="mr-4" htmlFor="" >Password</label>
-                    <input type="text" className="w-[250px]"/>
+                    <input type="text" className="w-[250px] px-2 text-dark-grey" onChange={(e) => setPassword(e.target.value)}/>
                 </p>
                 <div className="grid justify-center gap-2 mb-4 text-white">
                     <button className="rounded-lg bg-deep-purple w-[250px] h-[50px] shadow-sm mb-2" type="sumbit">Login</button>
