c488dab4ba2b1efdfb0a96cfd52d93ae0f2e48c0
ejcanoy <euan.james.canoy@gmail.com>
2024-07-16 18:50:02 -0700

    Added modal pop up

--- src/pages/components/navbar.jsx
+++ src/pages/components/navbar.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { LogOut, MessageCircle, Settings, Users } from "react-feather";
 import { Link, useLocation, useNavigate, useParams } from "react-router-dom";
 
@@ -6,11 +7,19 @@ function Navbar() {
     const { chatId, profileId } = useParams();
     const navigate = useNavigate();
 
+    const [displayProfileModal, setDisplayProfileModal] = useState(false);
+
+    function handleProfileClick() {
+        setDisplayProfileModal(!displayProfileModal);
+    }
+
     function handleLogOut() {
         localStorage.removeItem("token");
         alert("You have been logged out!");
         navigate("/");
     }
+
+
 
     return (
         <>
@@ -30,15 +39,17 @@ function Navbar() {
                     </Link>
                 </div>
                 <div className="relative">
-                    <button>
+                    <button onClick={() => handleProfileClick()}>
                         <div className="rounded-full h-12 w-12 md:h-16 md:w-16 bg-white"></div>
                     </button>
+                    {displayProfileModal &&
                     <div className="absolute top-0 -translate-y-48 -translate-x-full md:translate-x-1/4 w-48 h-48 rounded-lg p-2 bg-light-grey">
                         <button className="flex gap-2 items-center h-10"
                             ><div className="rounded-full h-8 w-8 bg-white grid place-items-center"><Settings /></div><span className="text-white">Settings</span></button>
                         <button className="flex gap-2 items-center h-10"
                             onClick={() => { handleLogOut() }}><div className="rounded-full h-8 w-8 bg-white grid place-items-center"><LogOut size={18} /></div><span className="text-white">Log Out</span></button>
                     </div>
+                    }
                 </div>
             </div>
         </>
